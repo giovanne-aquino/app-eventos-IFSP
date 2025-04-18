446230d0ac07af62599ff585f198ac3d627bdc59
LucasLoopsT <128094146+LucasLoopsT@users.noreply.github.com>
2025-04-18 19:09:37 -0300

    feat: form become component

--- src/components/utils/greenLine.jsx
+++ src/components/utils/greenLine.jsx
@@ -1,5 +1,9 @@
 function GreenLine() {
-  return <hr className="w-[70%] border-4 border-FI_green" />;
+  return (
+    <div className="w-full flex items-center justify-center">
+      <hr className="w-[70%] max-w-screen-lg border-2 border-FI_green" />
+    </div>
+  )
 }
 
 export default GreenLine;
