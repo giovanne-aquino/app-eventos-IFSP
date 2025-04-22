d9344371267c419e3e3ca318365acaf1ea6f25dc
Pedro Xavier Oliveira <pxo2005@gmail.com>
2025-04-21 22:51:04 -0300

    Final Component1

--- src/components/events/shortCard.jsx
+++ src/components/events/shortCard.jsx
@@ -1,20 +1,155 @@
 import React from "react";
 
-function ShortCard({ name }) {
+function ShortCard({ eventType, eventOrigin, title, titleLargeEvent, titleActivity, user, capacity, capacityLargeEvent, format, startHours, endHours, startDay, startMounth, endDay, endMounth }) {  
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+     < >
+
+    {eventType === "SIMPLE" ? ( 
+
+      <div className="flex flex-col items-center justify-center h-screen">
       {/* Primeira div */}
-      <div className="h-[15.875rem] w-[33rem] bg-stone-200 border border-gray-300 rounded-t-lg rounded-b-none p-4 flex flex-col justify-between">
-        <h2 className="text-lg font-semibold">{name}</h2>
-        <p className="text-gray-600">Data: 01/01/2023</p>
+      
+      <div className="h-[15.875rem] w-[33rem] bg-FI_neutral_30 border border-b-0 border-FI_neutral_30 rounded-t-lg rounded-b-none p-0 flex flex-col justify-between">
+
+        <div className="relative flex flex-col items-start justify-start h-full">
+          <div className="absolute bottom-0 w-[6.875rem] h-auto bg-FI_yellowDark p-2 border border-b-0 border-FI_yellowDark rounded-tr-lg flex items-center justify-center">
+            <p className="text-lg font-montserrat text-FI_neutral_0">{format}</p>
+          </div>
+        </div>
+
       </div>
 
       {/* Segunda div */}
-      <div className="h-[12.9375rem] w-[33rem] bg-white-200 border border-gray-300 rounded-b-lg rounded-t-none p-4 flex flex-col justify-between">
-        <h2 className=" text-3xl font-bold font-montserrat text-slate-800">{name}</h2>
-        <p className="text-gray-600">Data: 01/01/2023</p>
+      <div className="relative h-[12.9375rem] w-[33rem] bg-FI_neutral_0 border border-t-0 border-FI_neutral_30 rounded-b-lg rounded-t-none p-4 flex justify-between items-center gap-3 ">
+        {/* Texto e informações */}
+        <div className="flex flex-col items-start justify-start h-full">
+          <h2 className="text-3xl font-bold font-montserrat text-FI_green_dark mb-4">{title}</h2>
+          <div className="space-y-2">
+            <p className="text-base font-medium font-montserrat text-FI_neutral_90">{user}</p>
+            <p className="text-base font-medium font-montserrat text-FI_neutral_90">{capacity}</p>
+          </div>
+        </div>
+
+        
+        <div className="absolute w-[8.375rem] h-[7.0625rem] border border-FI_green rounded-lg p-4 flex flex-col justify-between items-center gap-2 bottom-0 right-0 m-4">
+
+          <div className=" flex justify-between items-center m-auto gap-2">
+
+            <div className="w-fit h-8 font-montserrat text-2xl  text-FI_neutral">{startDay}</div>
+            
+            <div className="w-fit h-8 font-montserrat text-2xl  text-FI_green_dark">{startMounth}</div>
+          
+          </div>
+
+          
+          <div className=" w-[5rem] h-[0.30rem] bg-FI_neutral_60 border rounded-sm"></div>
+          
+          <div className=" flex justify-between items-center">
+            <div className=" text-base">{startHours}-</div>
+            <div className=" text-base">{endHours}</div>
+          </div>
+
+        </div>
       </div>
     </div>
+        
+    ) : eventType === "LARGE" ? (
+
+      <div className="flex flex-col items-center justify-center h-screen">
+      {/* Primeira div */}
+      
+      <div className="h-[15.875rem] w-[33rem] bg-FI_neutral_30 border border-b-0 border-FI_neutral_30 rounded-t-lg rounded-b-none p-0 flex flex-col justify-between">
+
+        
+
+      </div>
+
+      {/* Segunda div */}
+      <div className="relative h-[12.9375rem] w-[33rem] bg-FI_neutral_0 border border-t-0 border-FI_neutral_30 rounded-b-lg rounded-t-none p-4 flex justify-between items-center gap-3 ">
+        {/* Texto e informações */}
+        <div className="flex flex-col items-start justify-start h-full">
+          <h2 className="text-3xl font-bold font-montserrat text-FI_green_dark mb-4">{titleLargeEvent}</h2>
+          <div className="space-y-2">
+            <p className="text-base font-medium font-montserrat text-FI_neutral_90">{user}</p>
+            <p className="text-base font-medium font-montserrat text-FI_neutral_90">{capacityLargeEvent}</p>
+          </div>
+        </div>
+
+        
+        <div className="absolute w-[8.375rem] h-[7.0625rem] border border-FI_green rounded-lg p-4 flex flex-col justify-between items-center gap-2 bottom-0 right-0 m-4">
+
+          <div className=" flex justify-between items-center m-auto gap-2">
+
+            <div className="w-fit h-8 font-montserrat text-xl  text-FI_neutral">{startDay}</div>
+            
+            <div className="w-fit h-8 font-montserrat text-xl  text-FI_green_dark">{startMounth}</div>
+          
+          </div>
+
+          
+          <div className=" w-20 h-2 bg-FI_neutral_60 border rounded-sm"></div>
+          
+          <div className=" flex justify-between items-center gap-2">
+            <div className=" w-fit h-8 font-montserrat text-xl  text-FI_neutral">{endDay}</div>
+            <div className=" w-fit h-8 font-montserrat text-xl  text-FI_green_dark">{endMounth}</div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    ) : (
+
+      <div className="flex flex-col items-center justify-center h-screen">
+      {/* Primeira div */}
+      
+      <div className="h-[15.875rem] w-[33rem] bg-FI_neutral_30 border border-b-0 border-FI_neutral_30 rounded-t-lg rounded-b-none p-0 flex flex-col justify-between">
+
+        <div className="relative flex flex-col items-start justify-start h-full">
+          <div className="absolute bottom-0 w-[6.875rem] h-auto bg-FI_yellowDark p-2 border border-b-0 border-FI_yellowDark rounded-tr-lg flex items-center justify-center">
+            <p className="text-lg font-montserrat text-FI_neutral_0">{format}</p>
+          </div>
+        </div>
+
+      </div>
+
+      {/* Segunda div */}
+      <div className="relative h-[12.9375rem] w-[33rem] bg-FI_neutral_0 border border-t-0 border-FI_neutral_30 rounded-b-lg rounded-t-none p-4 flex justify-between items-center gap-3 ">
+        {/* Texto e informações */}
+        <div className="flex flex-col items-start justify-start h-full">
+          <h2 className="text-3xl font-bold font-montserrat text-FI_green_dark mb-0">{titleActivity}</h2>
+          <p className="text-base font-bold font-montserrat text-FI_green mb-4">{eventOrigin}</p>
+
+          <div className="space-y-2">
+            <p className="text-base font-medium font-montserrat text-FI_neutral_90">{user}</p>
+            <p className="text-base font-medium font-montserrat text-FI_neutral_90">{capacity}</p>
+          </div>
+        </div>
+
+        
+        <div className="absolute w-[8.375rem] h-[7.0625rem] border border-FI_green rounded-lg p-4 flex flex-col justify-between items-center gap-2 bottom-0 right-0 m-4">
+
+          <div className=" flex justify-between items-center m-auto gap-2">
+
+            <div className="w-fit h-8 font-montserrat text-2xl  text-FI_neutral">{startDay}</div>
+            
+            <div className="w-fit h-8 font-montserrat text-2xl  text-FI_green_dark">{startMounth}</div>
+          
+          </div>
+
+          
+          <div className=" w-[5rem] h-[0.30rem] bg-FI_neutral_60 border rounded-sm"></div>
+          
+          <div className=" flex justify-between items-center">
+            <div className=" text-base">{startHours}-</div>
+            <div className=" text-base">{endHours}</div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    ) }
+    </>
   );
 }
 
