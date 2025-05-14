0817ad60227a35732a7e3fcb4e2b6eb714c9349f
unknown <eduardomssousa@hotmail.com>
2025-05-13 22:28:37 -0300

    Initial Commit

---
+++ src/app/ViewSubscribes/page.jsx
@@ -0,0 +1,10 @@
+export default function ViewSubscribes() {
+    return (
+        <div>
+            <h1>See Subscribes</h1>
+            <p>Esta página é um exemplo de como criar uma página para ver as inscrições.</p>
+            <p>Você pode usar este exemplo como base para criar sua própria página de inscrições.</p>
+            <p>Para acessar a página, vá para: http://localhost:3000/SeeSubscribes</p>
+        </div>
+    );
+}
